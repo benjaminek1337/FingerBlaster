fdd7ab6fd77eb73429e66482eabc5f5e4f2be92d
Benjamin Ek <benjaminek@live.se>
2020-10-06 12:28:21 +0200

    Added WPM counters

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ window.onload = function (){
     function onInit(){
         fillTextArea(); //Relik från tidigare. Ersätt mot aktiv text i listan
         fillTextArray();
+        endGameBtn.disabled = true;
     }
 
     function fillTextArea(){
@@ -35,17 +36,14 @@ window.onload = function (){
             char.className = ""
         }
         counter = 0;
-    }
-
-    function getPreviousChar(){
-        let previousChar = chars.find(c => c.id == char.id - 1);
-        return previousChar;
+        errros = 0;
     }
 
     function markActiveChar(){
         char = chars.find(c => c.id == counter);
         if(char.id != 0){
-            getPreviousChar().classList.remove("active-char");
+            let previousChar = chars.find(c => c.id == char.id - 1);
+            previousChar.classList.remove("active-char");
         }
         char.classList.add("active-char");
     }
@@ -55,8 +53,24 @@ window.onload = function (){
             char.classList.add("correct-char");
         } else {
             char.classList.add("incorrect-char");
+            errors++;
+            setNrOfErrorsText();
         }
+    }
 
+    function getWPM(){
+        let elapsedTime = (currentTime - startTime) / 60000;
+        let grossWPM = (counter / 5) / elapsedTime;
+        let netWPM = grossWPM - ( errors / 60000);
+        let wpmText = document.getElementById("wpm-text");
+        let netWPMText = document.getElementById("net-wpm-text");
+        wpmText.innerHTML ="Total WPM: " + Math.ceil(grossWPM);
+        netWPMText.innerHTML ="Justerad WPM: " + Math.ceil(netWPM);
+    }
+
+    function setNrOfErrorsText(){
+        let errorsText = document.getElementById("errors-text");
+        errorsText.innerHTML = "Antal fel: " + errors;
     }
 
     var textarea = document.getElementById("game-textarea");
@@ -64,27 +78,40 @@ window.onload = function (){
     var startGameBtn = document.getElementById("start-game-btn");
     var endGameBtn = document.getElementById("end-game-btn");
     let chars;
-    var counter = 0;
     var char;
-    var inputString = "Dä Börja likna nått nu";
+    var counter = 0;
+    var errors = 0;
+    var startTime;
+    var currentTime;
+    var inputString = "Dä Börja likna nått nu, för nu kommer det en hel " + 
+    "jävla blaffa text här som man ska matcha";
 
     startGameBtn.addEventListener("click", function(){
         startGame();
+        startGameBtn.disabled = true;
+        endGameBtn.disabled = false;
     })
     endGameBtn.addEventListener("click", function(){
         endGame();
+        startGameBtn.disabled = false;
+        endGameBtn.disabled = true;
     });
     textinput.addEventListener("keydown", function(event){
         var key = event.key;
+        if(startTime == null){
+            startTime = Date.now();
+            console.log(startTime);
+        }
         if(key == " "){
             textinput.value="";
         }
         if(key != "Shift"){
+            currentTime = Date.now();
             markCorrectChar(key);
             counter++;
             markActiveChar();
+            getWPM();
         }
-
     })
 
     window.addEventListener("load", onInit(), false);
